c0181ab6622b963fc9869d8e3b7d5fae76986add
foliest-123 <thegreatvijay1991@gmail.com>
2022-04-07 21:13:34 +0530

    about alt

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,22 +5,48 @@ const About = () => {
 
      <div className="about__container">
          <h1>About <span className="us">Us</span></h1>
+       
          <div className="about__content">
-    <p>     The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-         The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-         The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-  <br></br>
-        
-          The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-
-
-          The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-
-
-           The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-            The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-             The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
-             </p>
+    <p>     <span>All the students of the department are members of the Departmental Association,  </span>  
+    <span>Society for Advancement of Computer Science and Engineering. </span> 
+    <span>Further to sharpen and share the technical knowledge, all the students of Computer Science and Engineering are members of prestigious technical institutions such as </span>  
+     
+    <span>1. ISTE student’s chapter-Indian Society for Technical Education. </span>  
+    <span>2. IEI student’s chapter- Institution of Engineers India. </span>  
+    <span>All the students of the department are members of the Departmental Association,  </span>  
+    <span>Society for Advancement of Computer Science and Engineering. </span> 
+    <span>Further to sharpen and share the technical knowledge, all the students of Computer Science and Engineering are members of prestigious technical institutions such as </span>  
+     
+    <span>1. ISTE student’s chapter-Indian Society for Technical Education. </span>  
+    <span>2. IEI student’s chapter- Institution of Engineers India. </span>  
+    <span>All the students of the department are members of the Departmental Association,  </span>  
+    <span>Society for Advancement of Computer Science and Engineering. </span> 
+    <span>Further to sharpen and share the technical knowledge, all the students of Computer Science and Engineering are members of prestigious technical institutions such as </span>  
+     
+    <span>1. ISTE student’s chapter-Indian Society for Technical Education. </span>  
+    <span>2. IEI student’s chapter- Institution of Engineers India. </span>  
+     
+      </p>
+             {/* <p>
+  <span>GCE-Salem was started during the Third Plan period in 1966 and is located on a beautiful site surrounded by hills. Its area is 1.62 square kilometres (400 acres). This is the third government engineering institution formed in Tamil Nadu.[1] The first batch of students was selected for 1966 for courses in Civil Engineering</span>
+  <span>GCE-Salem was started during the Third Plan period in 1966 and is located on a beautiful site surrounded by hills. Its area is 1.62 square kilometres (400 acres). This is the third government engineering institution formed in Tamil Nadu.[1] The first batch of students was selected for 1966 for courses in Civil Engineering</span>
+  <span>GCE-Salem was started during the Third Plan period in 1966 and is located on a beautiful site surrounded by hills. Its area is 1.62 square kilometres (400 acres). This is the third government engineering institution formed in Tamil Nadu.[1] The first batch of students was selected for 1966 for courses in Civil Engineering</span>
+  <span>limits</span>
+  <span>to</span>
+  <span>what</span>
+  <span>you</span>
+  <span>can</span>
+  <span>accomplish,</span>
+  <span>except</span>
+  <span>the</span>
+  <span>limits</span>
+  <span>you</span>
+  <span>place</span>
+  <span>on</span>
+  <span>your</span>
+  <span>own</span>
+  <span>thinking.</span>
+</p> */}
          </div>
      </div>
 
