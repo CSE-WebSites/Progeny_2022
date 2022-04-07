63f19237103fbf23035178f6e367fdf86a534607
foliest-123 <thegreatvijay1991@gmail.com>
2022-04-07 20:25:29 +0530

    about alt

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,41 @@ import "./About.css"
 const About = () => {
     return ( 
         <div className="about">
-            about
-        </div>
+
+     <div className="about__container">
+         <h1>About <span className="us">Us</span></h1>
+         <div className="about__content">
+    <p>     The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+         The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+         The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+  <br></br>
+        
+          The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+
+
+          The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+
+
+           The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+            The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+             The Department Library has well stocked with Technical Books, newspapers, reference books and maintaining the previous year question papers for student references.
+             </p>
+         </div>
+     </div>
+
+
+
+
+
+            <div class="bg"></div>
+
+      <div class="star-field">
+       <div class="layer"></div>
+      <div class="layer"></div>
+     <div class="layer"></div>
+     </div>
+</div>
+        
      );
 }
  
