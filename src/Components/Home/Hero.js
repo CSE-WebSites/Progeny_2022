f012ba5c55f455b95633ececdb391ec0957e07e5
samapth-ops <sampathkaali002@gmail.com>
2022-04-11 17:00:32 +0530

    home changed added

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -7,8 +7,10 @@ const HeroHome = () => {
         <div className="hero_home">
             <section className="hero">
                 <div className="content">
-                    <h2>Progen<span className="i_progen">i</span> 22.0</h2>
-                    <p>It is a National Level technical Symposium conducted by Society for Advancement of Computer Science and Engineering (SACSE) in order to nurture and encourage young talents and innovations through different events.</p>
+                    <h2>GOVERNMENT COLLEGE OF ENGINEERING,</h2><h2>SALEM-11.</h2>
+                    <h3 className="dept_name">DEPARTMENT OF COMPUTER SCIENCE ENGINEERING.</h3>
+                    <h3>Progen<span className="i_progen">i</span> 22.0</h3>
+                    <p>It is a National Level Technical Symposium conducted by Society for Advancement of Computer Science and Engineering (SACSE) in order to nurture and encourage young talents and innovations through different events.</p>
                     <CountDown/>
                     <Count/>
                 </div>
