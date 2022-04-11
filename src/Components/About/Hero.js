a5d27920ed788cd47d44c11661a0d489ea56715a
sridevi14 <sridevimanjuraja@gmail.com>
2022-04-11 15:21:18 +0530

    new

---
+++ src/Components/About/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import './About.css'
+
+
+
+const Hero = () => {
+  return (
+    <div >
+      
+        
+
+
+
+      <div className="head_bg">
+
+    <div className="heroo">
+      <div className="wrapper">
+        <div className="hero_ele">
+          <p className="title">About Us</p>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+    </div>
+  )
+}
+
+export default Hero;
